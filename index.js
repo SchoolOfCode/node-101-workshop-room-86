c2cd3d4d0ced67a9eac085c808099ccf263eedb5
jamesgaynor <james_gaynor@btinternet.com>
2021-12-06 21:41:45 +0000

    task 2d done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let myCollection = [
       whatILike: "Eat. Sleep. Code. Repeat :)"
     }
   ];
-  console.log(myCollection);
+  //console.log(myCollection);
 
   //2c. Create a function called describeItem, which should take in an item as a parameter done //
   //(the argument handed to this function would be an item from our collection). 
@@ -27,11 +27,47 @@ For example, the result of calling the describeItem function with the first item
  "I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"*/
 
   function describeItem(item) {
-    if (item.count === 1 ){
-    console.log("I have a " + (myCollection[0].name) + ". Here's what I like about it:" + (myCollection[0].whatILike))
+    if (item.count > 1 ){
+    console.log("I have " + (item.name) + ". Here's what I like about them:" + (item.whatILike))
     } 
     else {
-        console.log("I have count names. Here's what I like about them: whatILike")
+        console.log("I have a " + (item.name) + ". Here's what I like about it:" + (item.whatILike))
+        
     }}
   //Call your function below where you've defined it, handing in the first item in myCollection
-  describeItem(myCollection[0])
+  //describeItem(myCollection[0])
+
+  // 👉 2d. Now make a function called describeCollection that takes in an array as a parameter. 
+  
+ function describeCollection(collection) {
+   collection.forEach(describeItem);
+ }
+ describeCollection(myCollection)
+  // for(let i = 0; i < array.length; i++)
+ // if( i<=3 ){ describeItem()
+  //}
+  //}
+//describeCollection[(myCollection)]
+
+  //The function should loop through the array, and for each item, it should call the describeItem 
+  //function so it displays a message according to how many you have in your collection.
+ /* 
+
+//taak 1b
+const legendaryCelebs = celebs.map(makeLegend)
+console.log(legendaryCelebs)
+
+//task 1c
+function startsWithVowel (celebs) {
+ return celebs.startsWith("A") || celebs.startsWith("E") 
+}
+
+let vowelCelebs = celebs.filter(startsWithVowel)
+console.log(vowelCelebs);
+
+
+
+Call your function below where you've defined it, handing in the myCollection array.
+
+Run the file again to see the output and check that it outputs the correct message for each item 
+in your collection (i.e. each object in your myCollection array).*/
