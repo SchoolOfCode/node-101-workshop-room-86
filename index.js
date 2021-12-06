2eda56bf8811f249c263f129ced399d5ab49dc53
jamesgaynor <james_gaynor@btinternet.com>
2021-12-06 18:17:37 +0000

    task 2c done

--- index.js
+++ index.js
@@ -18,24 +18,20 @@ let myCollection = [
   console.log(myCollection);
 
   //2c. Create a function called describeItem, which should take in an item as a parameter done //
+  //(the argument handed to this function would be an item from our collection). 
+    /* 
+  The function should console.log a message according to how many of the item you have.
+ Only one of the item in your collection,  log "I have a name. Here's what I like about it: whatILike". 
+ More than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
+For example, the result of calling the describeItem function with the first item in our collection would be:
+ "I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"*/
 
   function describeItem(item) {
-    if (myCollection.count === 1 ){
-    console.log("I have a name. Here's what I like about it: whatILike")
-    }
-    
+    if (item.count === 1 ){
+    console.log("I have a " + (myCollection[0].name) + ". Here's what I like about it:" + (myCollection[0].whatILike))
+    } 
     else {
         console.log("I have count names. Here's what I like about them: whatILike")
-
-  }}
-  describeItem([0])
-  /* (the argument handed to this function would be an item from our collection). 
-  The function should console.log a message according to how many of the item you have.
-
-If there's only one of the item in your collection, it should log "I have a name. Here's what I like about it: whatILike". 
-If you have more than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
-
-For example, the result of calling the describeItem function with the first item in our collection would be:
- "I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"
-
-Call your function below where you've defined it, handing in the first item in myCollection.*/
+    }}
+  //Call your function below where you've defined it, handing in the first item in myCollection
+  describeItem(myCollection[0])
