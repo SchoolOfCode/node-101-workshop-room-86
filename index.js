3cd95e50f21fb38ac169c06248bfd80e763d0d8f
jamesgaynor <james_gaynor@btinternet.com>
2021-12-06 22:04:59 +0000

    task 3 done

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
+import myCollection from "./collection.js";
   //console.log(myCollection);
 
   //2c. Create a function called describeItem, which should take in an item as a parameter done //
